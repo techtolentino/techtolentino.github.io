ee3c0640625d5ff14228ff67044ede8799c132f4
Randy Tolentino <makertolentino@gmail.com>
2014-07-10 11:14:09 -0500

    leave it for now

--- js/main.js
+++ js/main.js
@@ -38,6 +38,7 @@ $(document).ready(function(){
         },
         end: function() {
             //I get fired when the animation is ending
+            $top_nav.slideUp();
         },
         scrollChange: function() {
             //I get fired when you enter a section and I pass the list item of the section
