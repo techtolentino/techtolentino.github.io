f2df8cadafe51c9d293c2176437bb3e016f7e67e
Randy Tolentino <makertolentino@gmail.com>
2014-11-14 05:41:24 -0600

    no resume

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 (function ($) {
 // VERTICALLY ALIGN FUNCTION
-$.fn.vAlign = function() {
+  $.fn.vAlign = function() {
     return this.each(function(i){
     var ah = $(this).height();
     var ph = $(this).parent().height();
     var mh = Math.ceil((ph-ah) / 2);
     $(this).css('padding-top', mh);
     });
-};
+  };
 
 })(jQuery);
 $(document).ready(function(){
@@ -18,31 +18,29 @@ $(document).ready(function(){
 	}
 
 	setHeight();
-	/*$(".slogan h1").slabText({
-	        "viewportBreakpoint":300
-	});*/
 	
 	$(window).bind('resize',function() {
 		setHeight();
-	 });
+  });
 
-	 $('#top-nav').onePageNav({
+	$('#top-nav').onePageNav({
 
-        currentClass: 'current',
-        changeHash: false,
-        scrollSpeed: 750,
-        scrollOffset: 50,
-        scrollThreshold: 0.5,
-        filter: ':not(.external)',
-        begin: function() {
-            //I get fired when the animation is starting
-        },
-        end: function() {
-            //I get fired when the animation is ending
-            // $top_nav.slideUp();
-        },
-        scrollChange: function() {
-            //I get fired when you enter a section and I pass the list item of the section
-        }
-    });
+      currentClass: 'current',
+      changeHash: false,
+      scrollSpeed: 750,
+      scrollOffset: 50,
+      scrollThreshold: 0.5,
+      filter: ':not(.external)',
+      begin: function() {
+          //I get fired when the animation is starting
+      },
+      end: function() {
+          //I get fired when the animation is ending
+          // $top_nav.slideUp();
+      },
+      scrollChange: function() {
+          //I get fired when you enter a section and I pass the list item of the section
+      }
+  });
+
 });
